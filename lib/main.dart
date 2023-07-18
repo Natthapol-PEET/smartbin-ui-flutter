d6a5598465aad4677ef9efb2227867507c26ff6c
Natthapol-PEET <natthapol.nonthasri@krungsri.com>
2023-07-18 11:04:32 +0700

    build home

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       initialBinding: RootBinding(),
-      initialRoute: RoutePath.video,
+      initialRoute: RoutePath.home,
       getPages: AppRout.router,
     );
   }
